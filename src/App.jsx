f8a2d1fcb22ef765129b88e25b623d5d288879c2
Nerma5 <nermahh5@gmail.com>
2023-04-17 16:18:24 +0200

    removed folder cardData

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
-import cardData from './components/Card/cardData/cardData'
 import { FaGithub, FaJs, FaLinkedin, FaSlack } from 'react-icons/fa';
+
+const cardData =[
+  {
+    id:1,
+    name: 'Nerma',
+    login: 'nermahh5',
+    web: 'GitHub',
+    repo: '25 repositories',
+    icons: <FaGithub/>
+  },
+  {
+    id:2,
+    name: 'Anes',
+    login: 'anesdolicanin',
+    web: 'Js',
+    repo: ' 30 projects',
+    icons: <FaJs />
+  },
+  {
+    id:3,
+    name: 'Hamza',
+    login: 'hamzaat5',
+    web: 'Linkedn',
+    repo: '500+ conections',
+    icons: <FaLinkedin />
+  },
+  {
+    id:4,
+    name: 'Ensar',
+    login: 'ensargit2',
+    web: 'Slack',
+    repo: '20 servers',
+    icons: <FaSlack />
+  }
+]
+
+
 
 
 function App() {
